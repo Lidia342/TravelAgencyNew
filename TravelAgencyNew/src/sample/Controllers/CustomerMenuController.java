5d0e727ac6dbe8dffb20c2c340e411eb8ff880f3
Aymen Safi <oussamamhsafi@gmail.com>
2020-05-21 09:49:53 +0200

    fixing email and lots of other things

--- TravelAgencyNew/src/sample/Controllers/CustomerMenuController.java
+++ TravelAgencyNew/src/sample/Controllers/CustomerMenuController.java
@@ -2,11 +2,21 @@ package sample.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import sample.Model.Data;
 import sample.Model.SceneSwitcher;
 
-import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomerMenuController {
+public class CustomerMenuController implements Initializable {
+
+    @FXML Button loginButton;
+    @FXML Button editInfoButton;
+
+
+    Data myData= Data.getInstance();
 
     @FXML public void viewAndBookPackage(ActionEvent ae){
         SceneSwitcher.SwitchScene(ae, "../View/ViewPackage.fxml");
@@ -20,4 +30,27 @@ public class CustomerMenuController {
 
         SceneSwitcher.SwitchScene(ae, "../View/ViewPackagesScene.fxml");
     }
+
+    @FXML public void signUpAction(ActionEvent ae){
+        SceneSwitcher.SwitchScene(ae, "../View/SignUp.fxml");
+    }
+
+    @FXML public void loginAction(ActionEvent ae){
+        SceneSwitcher.SwitchScene(ae, "../View/Login.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        if(myData.getUser()!=null){
+
+            editInfoButton.setVisible(true);
+            loginButton.setVisible(false);
+        }
+        else{
+            editInfoButton.setVisible(false);
+            loginButton.setVisible(true);
+        }
+    }
+
 }
